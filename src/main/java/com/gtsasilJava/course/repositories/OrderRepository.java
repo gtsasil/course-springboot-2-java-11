0b1358fdd30f39fac5d8ecc3a662648e5d070bc8
gtsasil <gtsasil@gmail.com>
2020-04-21 18:41:29 +0100

    order, Instant, Iso 8601 PART 2

---
+++ src/main/java/com/gtsasilJava/course/repositories/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.gtsasilJava.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gtsasilJava.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+	
+	
+
+}
