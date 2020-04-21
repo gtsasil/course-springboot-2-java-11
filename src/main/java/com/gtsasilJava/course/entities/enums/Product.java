c11737aa9783cac40cf5d71dbef98df3a65f7541
gtsasil <gtsasil@gmail.com>
2020-04-21 20:35:17 +0100

    Product Ent

--- src/main/java/com/gtsasilJava/course/entities/enums/Product.java
+++ src/main/java/com/gtsasilJava/course/entities/enums/Product.java
@@ -1,4 +1,4 @@
-package com.gtsasilJava.course.entities;
+package com.gtsasilJava.course.entities.enums;
 
 import java.io.Serializable;
 import java.util.HashSet;
@@ -11,63 +11,79 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
-import com.gtsasilJava.course.entities.enums.Product;
+import com.gtsasilJava.course.entities.Category;
 
 @Entity
-@Table(name = "tb_category")
-public class Category implements Serializable{
+@Table(name  = "tb_Product")
+public class Product implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	private String description;
+	private double price;
+	private String imgUrl;
 	
 	@Transient
-	private Set<Product> products = new HashSet<>();
+	private Set<Category> categories = new HashSet<>();
 	
-	public Category() {
+	public Product() {
 	}
 
-	public Category(Long id, String name) {
+	public Product(Long id, String name, String description, double price, String imgUrl) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
 	}
-
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getImgUrl() {
+		return imgUrl;
+	}
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
-	
-	public Set<Product> getProducts() {
-		return products;
+	public Set<Category> getCategories() {
+		return categories;
 	}
-
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		return result;
 	}
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -76,17 +92,14 @@ public class Category implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
 		return true;
 	}
+
+	
 }
---
+++ src/main/java/com/gtsasilJava/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.gtsasilJava.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gtsasilJava.course.entities.enums.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+}
